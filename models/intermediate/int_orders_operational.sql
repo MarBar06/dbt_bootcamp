69ec2081e9dd4079d05f463604860ef4214d5bd8
Mariana Barreto <marianabarretop7@gmail.com>
2024-02-07 16:59:49 +0000

    Rebuilt of int_sales_margin

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,14 +1,16 @@
 --Operational margin = margin + shipping fee - log_cost - ship_cost
 
-SELECT 
-date_date,
-orders_id, 
-revenue, 
-margin, 
-shipping_fee, 
-logcost, 
-ship_cost, 
-(margin + shipping_fee - logcost - CAST(ship_cost as float64)) AS operational_margin
+SELECT  
+date_date
+, orders_id 
+, SUM(revenue) AS revenue 
+, SUM(margin) AS margin 
+, SUM(shipping_fee) AS shipping_fee
+, SUM(logcost) AS logcost 
+, SUM(CAST(ship_cost as float64)) AS ship_cost
+, SUM(margin)+SUM(shipping_fee)-SUM(logcost)-SUM(CAST(ship_cost as float64)) AS operational_margin
 FROM {{ref("stg_raw__ship")}}
 JOIN {{ref("int_sales_margin")}}
 USING (orders_id)
+GROUP BY orders_id, date_date
+ORDER BY orders_id DESC
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,9 @@
-SELECT orders_id, date_date, revenue, quantity, 
-CAST(purchase_price AS float64) * quantity AS purchase_cost
-, revenue - (CAST(purchase_price AS float64) * quantity) AS margin
+SELECT orders_id, date_date
+,SUM(revenue) AS revenue
+,SUM(quantity) AS quantity
+,SUM(CAST(purchase_price AS float64) * quantity) AS purchase_cost
+,SUM(revenue - (CAST(purchase_price AS float64) * quantity)) AS margin
 FROM {{ref("stg_raw__sales")}}
 JOIN {{ref("stg_raw__product")}}
 USING (products_id)
+GROUP BY orders_id, date_date
